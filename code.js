18dffc828b1607b6967123a7776ee29749bb343b
VicSpl <victoria.spieler@gmail.com>
2020-11-12 02:31:17 +0100

    Variable : Créer et initialiser.

---
+++ code.js
@@ -0,0 +1,20 @@
+/*Premier exercice*/
+
+// Create your variables here
+// ==========================================
+let numberOfSeasons = 8;
+let numberOfEpisodes = 10;
+// ==========================================
+
+/*Deuxième exercice*/
+//===========================================
+let numberOfSeasons = 8;
+let numberOfEpisodes = 10; 
+let episodeTime = 45;
+let commercialTime = 5; 
+//===========================================
+
+//===========================================
+let totalShowTime = numberOfSeasons * numberOfEpisodes * (episodeTime + commercialTime)
+// ==========================================
+
