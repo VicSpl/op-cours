72012af6f98ab7fc1bae75ead5e30f6fe69a4d1e
VicSpl <victoria.spieler@gmail.com>
2020-11-25 10:05:48 +0100

    créer une variable, modifier une variable, créer une constante, type de variable (number, string, booléan), créer des objets et class.

--- code.js
+++ code.js
@@ -24,3 +24,36 @@ let episodeTitle = 'Odin le redoutable';
 let episodeDuration = 48;
 let hasBeenWatched = true;
 // =========================================
+
+/* Quatrième exercice */
+// =========================================
+let episode = {
+    title: "odin le surpuissant",
+    duration: 45,
+    hasBeenWatched: true
+  }; 
+//   ======================================
+
+/*Cinquième exercice */
+// =====================================
+let  episodeTitle = episode.Title;
+let episodeDuration = episode.duration;
+let episodeHasBeenWatched = episode.hasBeenWatched;
+// =====================================
+
+/* Sixième exercice*/
+// Create a class here
+// =====================================
+class episode {
+  constructor (title, duration, hasBeenWatched) {
+    this.title = title;
+    this.duration = duration;
+    this.hasBeenWatched = hasBeenWatched;
+  }
+    
+} let firstEpisode = new episode("le chat surpuissant", 45, true);
+  let secondEpisode = new episode("Les souris redoutable", 50, true);
+  let thirdEpisode = new episode("Le moustique sauvage", 50, false);  
+
+console.log(firstEpisode,secondEpisode,thirdEpisode);
+// =====================================
